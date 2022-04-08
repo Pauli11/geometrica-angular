ff3a00f781237b8fb3bdd106340f1651c4b2e927
Pauli11 <pauli.smendes11@gmail.com>
2022-04-07 21:59:28 -0300

    quarto commit - home ok, falta form na tela login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import{HomeComponent} from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
 
 
 const routes: Routes = [
- {path: "", component: HomeComponent}
+ {path: "", component: HomeComponent},
+ {path:"login", component:LoginComponent} 
  
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent  
   
   ],
   imports: [
